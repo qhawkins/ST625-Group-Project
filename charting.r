3a7f14d55cbeacdcd9dc7786df90c799b2aee171
qhawkins <qhawkins2002@gmail.com>
2025-05-05 14:29:27 -0400

    plotting

---
+++ charting.r
@@ -0,0 +1,45 @@
+file_path <- "C:/Users/quinn/Downloads/ST 625 - Group Project(New Data).csv"
+data <- read.csv(file_path)
+
+#directory to save plots
+plot_dir <- "plots"
+if (!dir.exists(plot_dir)) {
+  dir.create(plot_dir)
+  print("created plot directory")
+} else {
+  print("plot directory already exists")
+}
+
+response_var <- "R.PA"
+
+data$LgF <- ifelse(data$Lg == "NL", 1, 0)
+print("LgF")
+data$BatsRight <- ifelse(data$Bats == "Right", 1, 0)
+print("Bats")
+data$ThrowsRight <- ifelse(data$Throws == "Right", 1, 0)
+print("Throws")
+
+data$BirthplaceF <- as.factor(data$Birthplace)
+print("BirthplaceF")
+
+#nice color pallette, thanks https://htmlcolorcodes.com/
+plot_colors <- c(lightblue = "#87CEEB", darkorange = "#FA8072", beige = "#F5DEB3",
+                 lightpurple = "#D8BFD8", lightgreen = "#90EE90", lightred = "#F08080",
+                 orange = "#FFA500", pink="#FFB6C1", mediumblue="#6495ED")
+
+#univariate distribution plots
+png(filename = file.path(plot_dir, "univariate_distributions.png"), width = 1920, height = 1080)
+par(mfrow=c(1, 3))
+
+hist(data$Age, main="Distribution of Player Age", xlab="Size",
+     col=plot_colors["lightblue"], breaks=25, probability = TRUE)
+
+#distribution of weight
+hist(data$Weight, main="Distribution of Player Weight", xlab="Weight (lbs)",
+     col=plot_colors["lightred"], breaks=25, probability = TRUE)
+
+#distribution of height
+hist(data$Height, main="Distribution of Player Height", xlab="Height (inches)",
+     col=plot_colors["lightgreen"], breaks=25, probability = TRUE)
+
+dev.off()
